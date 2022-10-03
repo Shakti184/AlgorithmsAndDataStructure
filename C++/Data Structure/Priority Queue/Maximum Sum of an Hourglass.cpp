c65dc31c89453e5d984cada20815bf02aaec8db5
Shakti Maddheshiya <97889623+Shakti184@users.noreply.github.com>
2022-10-04 02:14:59 +0530

    Create Maximum Sum of an Hourglass.cpp

---
+++ C++/Data Structure/Priority Queue/Maximum Sum of an Hourglass.cpp
@@ -0,0 +1,22 @@
+//problem statement  https://leetcode.com/problems/maximum-sum-of-an-hourglass/
+
+class Solution {
+public:
+    int maxSum(vector<vector<int>>& g) {
+        int mx=0, m=g.size(),n=g[0].size();
+        for(int i=0;i<=m-3;i++){
+            for(int j=0;j<=n-3;j++){
+                int sum=0;
+                for(int k=i;k<i+3;k++){
+                    for(int l=j;l<j+3;l++){
+                        sum+=g[k][l];
+                    }
+                }
+                sum-=g[i+1][j];
+                sum-=g[i+1][j+2];
+                mx=max(sum,mx);
+            }
+        }
+        return mx;
+    }
+};
